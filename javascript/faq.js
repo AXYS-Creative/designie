1c0fb5fa210ef88c10bafd7505d0365ce2d6d7d2
axys-creative <axyscreativeagency@gmail.com>
2023-10-30 15:23:29 -0600

    resizing elements

--- javascript/faq.js
+++ javascript/faq.js
@@ -1,16 +1,12 @@
 export function faq() {
-    const faqQuestion = document.querySelectorAll(".question");
-    const faqAnswer = document.querySelectorAll(".answer");
-    
-    faqQuestion.forEach((faq, index) => {
-        faq.addEventListener("click", () => {
-            faqAnswer[index].classList.toggle("active");
-        });
+  const faqBlock = document.querySelectorAll(".question-block");
+  const faqAnswer = document.querySelectorAll(".answer");
+
+  faqBlock.forEach((faq, index) => {
+    faq.addEventListener("click", () => {
+      faqAnswer[index].classList.toggle("active");
     });
+  });
 }
 
 faq();
-
-
-
-
